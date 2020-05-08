9bd174d55b24424fdf268553f3769adf2f9efd0d
Sam Youatt <sdyouatt@btinternet.com>
2020-05-08 11:38:26 +0100

    Comments

--- GUI.py
+++ GUI.py
@@ -5,6 +5,7 @@ pygame.font.init()
 
 class Grid:
     def __init__(self, rows, cols, width, height, win):
+        # Create the random board
         board = createBoard(self, rows)
         self.board = board
         self.rows = rows
@@ -17,9 +18,11 @@ class Grid:
         self.model = None
         self.updateModel()
 
+
     def updateModel(self):
         self.model = [[self.squares[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
+    # Draws the complete board to the screen
     def drawBoard(self):
         # Draw grid
         gap = self.width / 9
@@ -38,16 +41,19 @@ class Grid:
             for j in range(self.cols):
                 self.squares[i][j].draw(self.win)
 
+    # Sets the temporary value for a square
     def setSquareTemp(self, val):
         row, col = self.selected
         self.squares[row][col].setTemp(val)
 
+    # Sets the actual value for a square
     def setSquare(self, val):
         row, col = self.selected
         if self.squares[row][col].value == 0:
             self.squares[row][col].set(val)
             self.updateModel()
 
+    # Calculates which square has been clicked in
     def click(self, pos):
         if pos[0] < self.width and pos[1] < self.height:
             gap = self.width / 9
@@ -57,6 +63,7 @@ class Grid:
         else:
             return None
 
+    # Selects the square that has been clicked or being used by the solver
     def select(self, row, col):
         # Resets previous selection
         for i in range(self.rows):
@@ -66,11 +73,13 @@ class Grid:
         self.squares[row][col].selected = True
         self.selected = (row, col)
 
+    # Clears any temp or actual values in the square
     def clear(self):
         row, col = self.selected
         self.squares[row][col].set(0)
         self.squares[row][col].setTemp(0)
 
+    # Returns true if there are no empty squares
     def isFinished(self):
         for i in range(self.rows):
             for j in range(self.cols):
@@ -78,6 +87,7 @@ class Grid:
                     return False
         return True
 
+    # The backtracking function
     def solve(self):
         find = findEmpty(self.model)
         if not find:
@@ -120,6 +130,7 @@ class Square:
         self.temp = 0
         self.selected = False
 
+    # Draws each square
     def draw(self, win):
         font = pygame.font.SysFont('arial', 40)
 
@@ -137,6 +148,7 @@ class Square:
         if self.selected:
             pygame.draw.rect(win, (255, 0, 0), (x,y, gap, gap), 3)
 
+    # Updates any changed values in the square
     def drawChange(self, win, green=True):
         font = pygame.font.SysFont("arial", 40)
 
@@ -154,19 +166,24 @@ class Square:
         else:
             pygame.draw.rect(win, (255, 0, 0), (x,y,gap,gap), 3)
 
+    # Sets the value of a square
     def set(self, val):
         self.value = val
     
+    # Sets the temp value for a square
     def setTemp(self, val):
         self.temp = val
 
+# Creates the randomised board
 def createBoard(self, rows):
     base = int(rows**0.5)
     side = base**2
 
+    # The pattern ensures random board each time
     def pattern(row, col):
         return (base*(row%base)+row//base+col)%side
 
+    # Shuffles the list of numbers and returns one
     def shuffle(s):
         return sample(s,len(s))
 
@@ -175,12 +192,11 @@ def createBoard(self, rows):
     cols = [g*base + c for g in shuffle(rBase) for c in shuffle(rBase)]
     nums = shuffle(range(1,base*base+1))
 
+    # Randomised the board
     board = [[nums[pattern(r,c)] for c in cols] for r in rows]
 
-    # Remove squares        
-    # number of empty squares
+    # Removes a certain proportion of squares
     squares = side*side
-    # lose a certain proportion of squares
     empties = squares * 1 // 2
 
     for p in sample(range(squares),empties):
@@ -188,6 +204,7 @@ def createBoard(self, rows):
 
     return board
 
+# Redraws the whole window to the screen
 def redrawWindow(win, board, time):
     win.fill((255,255,255))
     
@@ -199,6 +216,7 @@ def redrawWindow(win, board, time):
     # Draw board
     board.drawBoard()
 
+# Formats the time from seconds to minutes and seconds
 def formatTime(secs):
     seconds = secs%60
     minutes = seconds//60
@@ -207,6 +225,7 @@ def formatTime(secs):
 
     return time
 
+# Finds an empty square in the board, used by solver
 def findEmpty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
@@ -214,6 +233,7 @@ def findEmpty(board):
                 return (i, j)
     return None
 
+# Checks if the number is valid in a certain location
 def valid(board, num, pos):
     # check the row
     for i in range(len(board[0])):
@@ -247,6 +267,7 @@ def main():
     while run:
         playTime = round(time.time() - start)
 
+        # Handles all inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
